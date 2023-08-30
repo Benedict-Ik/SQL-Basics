2be04db86dc96581fc6d263e13bd6cf04f771f8d
Benedict Ik <benedict.ik98@gmail.com>
2023-08-30 16:24:14 +0100

    Initial

--- Default.session.sql
+++ Default.session.sql
@@ -1,65 +1,71 @@
--- Schema
-CREATE TABLE student(
+-- Whenever you create a DB, the first step is to define a schema
+-- A colon (;) represents an end to a statement
+-- W/O the colon, your code won't run.
+
+-- SCHEMA
+CREATE TABLE student (
     student_id INT PRIMARY KEY,
     student_name VARCHAR(20),
-    major VARCHAR(20)
+    major VARCHAR(20) 
 );
 
--- Option 2
-CREATE TABLE student(
+-- VERSION 2
+CREATE TABLE student (
     student_id INT PRIMARY KEY,
     student_name VARCHAR(20) NOT NULL,
-    major VARCHAR(20)
+    major VARCHAR(20) 
 );
 
--- Determining contents in a table
-DESCRIBE student;
-
--- Deleting Table
+-- DELETING TABLES
 DROP TABLE student;
 
--- Adding an extra column to an existing table
-ALTER TABLE student ADD gpa DECIMAL(10,2);
+-- KNOWING THE CONTENTS IN A TABLE
+DESCRIBE student;
 
--- Deleting an existing column in a Table
+-- ADDING AN EXTRA COLUMN TO AN EXISTING TABLE
+ALTER TABLE student ADD gpa DECIMA(3,2);
+
+-- DELETING AN EXISTING COLUMN FROM A TABLE
 ALTER TABLE student DROP COLUMN gpa;
 
--- Inserting data into Tables
-INSERT INTO student VALUES(
-    1, "John", "Chemistry"
-);
+-- INSERTING DATA INTO TABLE
+INSERT INTO student VALUES (1, "Jack", "English");
+INSERT INTO student VALUES (2, "Ben", "Maths");
+INSERT INTO student VALUES (3, "Ken", "Physics");
+INSERT INTO student VALUES (4, "Joy", "Chemistry");
+INSERT INTO student VALUES (5, "Rita", "Csc");
 
-INSERT INTO student VALUES(
-    2, "Ben", "Physics"
-);
+-- SPECIFYING COLUMN TO INSERT DATA INTO
+INSERT INTO STUDENT (student_id, student_name, major) VALUES (1, "Jack", "English");
+INSERT INTO STUDENT (student_id, student_name, major) VALUES (2, "Ben", "Maths");
+INSERT INTO STUDENT (student_id, student_name, major) VALUES (3, "Ken", "Physics");
+INSERT INTO STUDENT (student_id, student_name, major) VALUES (4, "Joy", "Chemistry");
+INSERT INTO STUDENT (student_id, student_name, major) VALUES (5, "Rita", "Csc");
 
-INSERT INTO student VALUES(
-    3, "Ken", "Maths"
-);
-
--- Inserting multiple data into Tables
+-- INSERTING MULTIPLE ROWS INTO TABLES
 INSERT INTO student VALUES
 (1, "John", "Chemistry"),
 (2, "Ben", "Physics"),
-(3, "Ken", "Maths"),
+(3, "Ken", "Maths"),	
 (4, "Jack", "Biology"),
 (5, "Rita", "Csc");
 
--- Inserting multiple data into Tables (Option 2)
-INSERT INTO table_name (column1, column2, column3,etc)
-VALUES 
-	(value1, value2, value3, etc),
-    (value1, value2, value3, etc),
-    (value1, value2, value3, etc);
+-- INSERTING MULTIPLE ROWS TO A TABLE (2)
+INSERT INTO STUDENT (student_id, student_name, major) VALUES 
+(1, "Jack", "English"),
+(2, "Ben", "Maths"),
+(3, "Ken", "Physics"),
+(4, "Joy", "Chemistry"),
+(5, "Rita", "Csc");
 
--- Inserting multiple data into Tables
-INSERT INTO student (student_id, student_name, major)
-VALUES 
-    (1, "John", "Chemistry"),
-    (2, "Ben", "Physics"),
-    (3, "Ken", "Maths"),
-    (4, "Jack", "Biology"),
-    (5, "Rita", "Csc");
+-- UPDATING SPECIFIC ROWS IN A TABLE
+UPDATE student
+SET major = "Bio" 
+WHERE major = "Biology";
 
--- Retrieving all data from table
+-- DELETING SPECIFIC ROWS IN A TABLE
+DELETE FROM student
+WHERE student_id = 5;
+
+-- RETRIEVING ALL DATA FROM A TABLE
 SELECT * FROM student;
