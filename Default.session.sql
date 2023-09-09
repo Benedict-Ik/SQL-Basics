b323d33e3c572c73da61ea5ebf46f394e17940d6
Benedict Ik <benedict.ik98@gmail.com>
2023-09-09 20:50:49 +0100

    latest commit. Added company DB; emp & brh table

--- Default.session.sql
+++ Default.session.sql
@@ -34,16 +34,70 @@ CREATE TABLE users(
     status VARCHAR(20)
 );
 
+-- COMPANY DB SCHEMA
+-- EMPLOYEES SCHEMA
+CREATE TABLE employees(
+    emp_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(40),
+    age INT,
+    sex VARCHAR(1),
+    salary DECIMAL(10,2),
+    super_id INT,
+    branch_id INT
+);
+
+-- BRANCH SCHEMA
+CREATE TABLE branch(
+    branch_id INT PRIMARY KEY AUTO_INCREMENT,
+    branch_name VARCHAR(40),
+    manager_id INT,
+    FOREIGN KEY(manager_id) REFERENCES employees(emp_id) ON DELETE SET NULL
+);
+
+-- CLIENT SCHEMA
+CREATE TABLE client(
+    client_id INT PRIMARY KEY AUTO_INCREMENT,
+    client_name VARCHAR(20),
+    branch_id INT FOREIGN
+);
+
+-- WORKS_WITH SCHEMA
+CREATE TABLE works_with(
+    emp_id INT PRIMARY KEY AUTO_INCREMENT,
+    client_id INT,
+    total_sales DECIMAL(10,2)
+);
+
+CREATE TABLE branch_supplier(
+    branch_id INT PRIMARY KEY AUTO_INCREMENT,
+    supplier_name VARCHAR(20),
+    supplier_type VARCHAR(20)
+);
+
 -- DETERMINING THE CONTENTS IN A TABLE
 DESCRIBE student;
 DESCRIBE users;
+DESCRIBE employees;
 
 -- DELETING TABLES
 DROP TABLE student;
 DROP TABLE users;
+DROP TABLE employees;
 
 -- ADDING AN EXTRA COLUMN TO AN EXISTING TABLE
 ALTER TABLE student ADD gpa DECIMA(3,2);
+
+-- ADDING BRANCH ID AS FOREIGN KEY TO EMPLOYEE TABLE
+ALTER TABLE employees
+ADD FOREIGN KEY(branch_id)
+REFERENCES branch(branch_id)
+ON DELETE SET NULL;
+
+-- ADDING SUPER ID AS FOREIGN KEY TO EMPLOYEE TABLE
+ALTER TABLE employees
+ADD FOREIGN KEY(super_id)
+REFERENCES employees(emp_id)
+ON DELETE SET NULL;
 
 -- DELETING AN EXISTING COLUMN FROM A TABLE
 ALTER TABLE student DROP COLUMN gpa;
