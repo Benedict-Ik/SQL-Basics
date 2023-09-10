5d56ca036426ef93337763d4c87b8b7cb77bc41e
Benedict Ik <benedict.ik98@gmail.com>
2023-09-10 20:47:35 +0100

    latest commit

--- Company DB Schema.sql
+++ Company DB Schema.sql
@@ -1,3 +1,5 @@
+-- NOTE THAT QUERIES BELOW DO NOT RUN SEQUENTIALLY BUT ARE EXECUTED ONE AT A TIME AND AT TIMES RANDOMLY
+
 -- COMPANY DB SCHEMA
 -- EMPLOYEES SCHEMA
 CREATE TABLE employees(
@@ -30,19 +32,22 @@ CREATE TABLE client(
 
 -- WORKS_WITH SCHEMA
 CREATE TABLE works_with(
-    emp_id INT PRIMARY KEY,
-    client_id INT PRIMARY KEY,
+    emp_id INT,
+    client_id INT,
     total_sales DECIMAL(10,2),
+    PRIMARY KEY(emp_id, client_id),
     FOREIGN KEY(emp_id) REFERENCES employees(emp_id) ON DELETE CASCADE,
     FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
 );
 
 CREATE TABLE branch_supplier(
-    branch_id INT PRIMARY KEY,
+    branch_id INT,
     supplier_name VARCHAR(40),
     supplier_type VARCHAR(40),
+    PRIMARY KEY(branch_id, supplier_name),
     FOREIGN KEY(branch_id)
-    ON DELETE SET CASCADE
+    REFERENCES branch(branch_id)
+    ON DELETE CASCADE
 );
 
 -- DETERMINING THE CONTENTS IN A TABLE
@@ -70,3 +75,39 @@ ALTER TABLE employees
 ADD FOREIGN KEY(super_id)
 REFERENCES employees(emp_id)
 ON DELETE SET NULL;
+
+-- INSERTING DATA INTO TABLES
+-- INSERTING DATA INTO EMPLOYEES TABLES
+-- INSERTING DATA FOR EMPLOYEES IN CORPORATE BRANCH
+INSERT INTO employees VALUES(100, "John Cooper", 50, "M", 390000.00, NULL, NULL);
+-- branch id had no values as no entry has been made for branch table
+INSERT INTO employees VALUES(101, "Pete Davidson", 28, "M", 280000.00, 100, 1);
+INSERT INTO employees VALUES(102, "Doyle Letterman", 45, "M", 190000.00, 100, 1);
+INSERT INTO employees VALUES(103, "Meghan Alex", 32, "F", 325000.00, 102, 1);
+INSERT INTO employees VALUES(104, "Andy Sherpherd", 27, "M", 200000.00, 102, NULL);
+-- Updating all other entries in abuja branch
+INSERT INTO employees VALUES
+(105, "Ashley Hudson", 40, "F", 360000.00, 102, 2),
+(106, "Carlos Sanchex", 41, "M", 215000.00, 100, 2);
+
+-- CREATING CORPORATE BRANCH
+INSERT INTO branch VALUES(1, "Corporate", 100);
+
+-- CREATING ABUJA BRANCH
+INSERT INTO branch VALUES(2, "abuja", 102);
+
+-- WE CAN THEN GO AHEAD AND UPDATE EMPLOYEES TABLE WITH CORPORATE'S BRANCH ID AS IT HAS NOW BEEN CREATED
+UPDATE employees
+SET branch_id = 1
+WHERE emp_id = 100;
+
+-- UPDATING EMPLOYEES TABLE WITH ABUJA'S BRANCH ID
+UPDATE employees
+SET branch_id = 2
+WHERE emp_id = 104;
+
+SELECT *
+FROM employees;
+
+SELECT *
+FROM branch;
